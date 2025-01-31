5215be574c6ce9937efdb79340977ab7cad2550f
mgjho <mingi.jho@kaist.ac.kr>
2025-02-01 08:29:20 +0900

    feat: add DisplayAccessor for interactive fit result visualization

--- src/xrfit/__init__.py
+++ src/xrfit/__init__.py
@@ -1,4 +1,5 @@
+from xrfit.display import DisplayAccessor
 from xrfit.fit import FitAccessor
 from xrfit.params import ParamsAccessor
 
-__all__ = ["FitAccessor", "ParamsAccessor"]
+__all__ = ["FitAccessor", "ParamsAccessor", "DisplayAccessor"]
