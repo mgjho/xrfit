7f1761f16cf0cdc4e35206041ab474efa067ca29
mgjho <mgjho96@gmail.com>
2025-02-03 10:56:06 +0900

    chore: reorder __all__ exports in __init__.py

--- src/xrfit/__init__.py
+++ src/xrfit/__init__.py
@@ -2,4 +2,4 @@ from xrfit.display import DisplayAccessor
 from xrfit.fit import FitAccessor
 from xrfit.params import ParamsAccessor
 
-__all__ = ["FitAccessor", "ParamsAccessor", "DisplayAccessor"]
+__all__ = ["DisplayAccessor", "FitAccessor", "ParamsAccessor"]
