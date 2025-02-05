b6c38015b829bf578eed1b6d1df7454d70f430bf
mgjho <mgjho96@gmail.com>
2025-02-05 10:51:46 +0900

    test: update get_test_data_2d

--- tests/test_fit.py
+++ tests/test_fit.py
@@ -7,16 +7,18 @@ from lmfit.models import LorentzianModel
 def get_test_data_2d():
     rng = np.random.default_rng(seed=0)
     x = np.linspace(-10, 10, 200)
-    y = np.linspace(-5, 5, 5)
+    y = np.linspace(-5, 5, 7)
     model = LorentzianModel()
     return xr.DataArray(
         np.stack(
             [
+                model.eval(x=x, amplitude=1, center=0, sigma=0.05 * 5),
                 model.eval(x=x, amplitude=1, center=0, sigma=0.1 * 5),
                 model.eval(x=x, amplitude=1, center=0, sigma=0.15 * 5),
                 model.eval(x=x, amplitude=1, center=0, sigma=0.2 * 5),
                 model.eval(x=x, amplitude=1, center=0, sigma=0.25 * 5),
                 model.eval(x=x, amplitude=1, center=0, sigma=0.3 * 5),
+                model.eval(x=x, amplitude=1, center=0, sigma=0.35 * 5),
             ]
         ).T
         + rng.normal(size=(x.size, y.size)) * 0.01,
